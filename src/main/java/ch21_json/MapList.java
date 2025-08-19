4dddefbd5a07aa9f153a840451785259a6f6a56a
Seo-051 <kingdom5437@naver.com>
2025-08-19 17:16:15 +0900

    feat 20250819

--- src/main/java/ch21_json/MapList.java
+++ src/main/java/ch21_json/MapList.java
@@ -1,29 +1,31 @@
+package ch21_json;
+
 import java.util.*;
 
 public class MapList {
     public static void main(String[] args) {
         List<Map<String, Object>> customers = new ArrayList<Map<String, Object>>();
 
-        // 예시 고객 정보 추가
         Map<String, Object> customer1 = new HashMap<>();
         customer1.put("name", "홍길동");
+        customer1.put("rating", "vip");
         customer1.put("age", 30);
-        customer1.put("email", "hong@example.com");
 
         Map<String, Object> customer2 = new HashMap<>();
-        customer2.put("name", "김철수");
-        customer2.put("age", 25);
-        customer2.put("email", "kim@example.com");
+        customer2.put("name", "김기영");
+        customer2.put("rating", "gold");
+        customer2.put("age", 35);
 
         customers.add(customer1);
         customers.add(customer2);
 
-        // 출력
         for (Map<String, Object> customer : customers) {
-            System.out.println("이름: " + customer.get("name"));
-            System.out.println("나이: " + customer.get("age"));
-            System.out.println("이메일: " + customer.get("email"));
-            System.out.println("------------");
+            System.out.println("이름 : " + customer.get("name"));
+            System.out.println("등급 : " + customer.get("rating"));
+            System.out.println("나이 : " + customer.get("age"));
         }
+
+
+
     }
 }
--- src/main/java/ch23_lambda/Calculator.java
+++ src/main/java/ch23_lambda/Calculator.java
@@ -1,4 +1,6 @@
 package ch23_lambda;
 
-public class Calculator {
+@FunctionalInterface
+public interface Calculator {
+    int calculate(int a, int b);
 }
